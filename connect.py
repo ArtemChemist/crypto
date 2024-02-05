d0b01848af6119ebba9442b9844d3e2dd56ab2c4
ArtemChemist <lebedevfedora@gmail.com>
2024-02-05 12:36:34 -0500

    read historic rate into pandas

--- connect.py
+++ connect.py
@@ -8,6 +8,8 @@ cb_secret = contents[1].split(':')[-1].strip()
 
 public_client = cbpro.PublicClient()
 
-result = public_client.get_product_24hr_stats('BTC-USDT')
+result = public_client.get_product_historic_rates('BTC-USDT', '2023-01-01', '2023-06-01', 86400)
+columns = [ 'time', 'low', 'high', 'open', 'close', 'volume' ]
+
 
 print(result)
