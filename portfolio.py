1ac650013692fd9e8d0e96d3201627e96e25cfa2
ArtemChemist <lebedevfedora@gmail.com>
2024-04-15 17:33:45 -0400

    Reading coinbase data into Assets

--- portfolio.py
+++ portfolio.py
@@ -1,16 +1,13 @@
 import pandas as pd
+import tensorflow as tf
 
 class Asset:
     def __init__(self, ticker):
-        self.price = 0
         self.ticker = ticker
         self.history = pd.DataFrame(columns = ['date_time', 'high', 'low', 'open', 'close', 'volume'])
-
-    def set_price(self, price):
-        self.price = price
     
     def latest_price(self):
-        return self.price
+        return self.history.iloc[-1]['close']
     
     def update_history(self, incoming_df:pd.DataFrame):
         '''
@@ -21,6 +18,7 @@ class Asset:
         df_to_use.set_index('date_time', inplace=True)
         self.history = self.history.combine_first(df_to_use)
         self.history.reset_index(inplace=True)
+        self.history.sort_values('date_time', ascending=True)
 
 
 class Portfolio:
@@ -34,6 +32,16 @@ class Portfolio:
     def get_value(self):
         self.value = sum([asset.latest_price() * qty for asset, qty in self.assets.items()])
         return self.value
+    
+class Strategy:
+
+    def __init__(self, model):
+        self.frequency = 1
+        self.model = model
+
+    def backtest(self, hist_data: pd.DataFrame, portfolio: Portfolio):
+        updated_portfolio = Portfolio()
+        return updated_portfolio
 
 
 
