997d6f40dbe2406b943ec7dffc452ed3a236dcb1
Artem Lebedev <lebedevfedora@gmail.com>
2024-04-22 23:12:25 -0400

    Moved all asset methods into class

--- portfolio.py
+++ portfolio.py
@@ -2,21 +2,52 @@ import pandas as pd
 import tensorflow as tf
 from pandas import Timestamp as tmpstemp
 from pandas import Timedelta as tmpdelta
+import cbpro
+import os
 
 class Asset:
     asset_dict = {}
+    public_client = cbpro.PublicClient()
+
+    @staticmethod
+    def connect_to_exchange():
+        with open('CoinBaseAPIKey.key', 'r') as f:
+            contents = f.readlines()
+        cb_key = contents[0].split(':')[-1].strip()
+        cb_secret = contents[1].split(':')[-1].strip()
+        #return Asset.public_client = cbpro.PublicClient()
+    
+    @staticmethod
+    def make_USD():
+        USD = Asset('USD')
+        date_range = pd.date_range(start=tmpstemp.fromisoformat('2000-01-01'), end=tmpstemp.today())
+        idx =pd.DatetimeIndex(date_range, name = 'date_time')
+        USD_2000 = pd.DataFrame({'low':[1] * len(idx), 'high':[1] * len(idx),
+                            'open':[1] * len(idx), 'close':[1] * len(idx),
+                            'volume':[1000]* len(idx)},
+                            index=idx)
+        USD.update_history_from_df(USD_2000)
+        return USD
 
     def __init__(self, ticker):
         self.ticker = ticker
+        self.local_path = f'{self.ticker}_history.csv'
+        Asset.asset_dict.pop(ticker, None)
         Asset.asset_dict[ticker] = self
         self.history = pd.DataFrame(columns = ['high', 'low', 'open', 'close', 'volume'],
                                     index = pd.DatetimeIndex([], name = 'date_time'))
+        try:
+            self.read_history_from_local()
+        except OSError:
+            pass
+
+        
     def latest_price(self):
         return self.price_on_date()
     
     def price_on_date(self, on_date = tmpstemp.today):
         '''
-        Returns the prcei on the date that is the closest to the supplied date
+        Returns the prce on the date that is the closest to the supplied date
         '''
         matches = self.history.index.get_indexer([on_date], method='nearest')
         matched_date = self.history.index[matches[0]]
@@ -24,11 +55,45 @@ class Asset:
             print(f'Reported {self.ticker} price is {on_date- matched_date} old')
         return self.history['close'].loc[matched_date]
     
-    def update_history(self, incoming_df:pd.DataFrame):
+    def update_history_from_df(self, incoming_df:pd.DataFrame):
         '''
         incoming_df: the dataframe with the new historical records
         '''
         self.history = pd.concat([self.history, incoming_df]).groupby(level=0).last()
+        self.save_history_to_local()
+
+    def update_history_from_excahnge(self, currency: str, date_from, date_to):
+        '''
+        dates as pd.Datetime
+        currency: the currency we pay to obtain this asset, as a ticker (USD, USDT etc)
+        '''
+        next_date = date_from
+        while next_date < date_to:
+            date_from_str = date_from.strftime('%Y-%m-%d')
+            next_date = min((date_from + tmpdelta(days=190)), date_to)
+            next_date_str = next_date.strftime('%Y-%m-%d')
+            incoming_data= pd.DataFrame(
+                Asset.public_client.get_product_historic_rates(f'{self.ticker}-{currency}',
+                                                        date_from_str, next_date_str, 86400)
+            )
+            incoming_data.columns = [ 'date_time', 'low', 'high', 'open', 'close', 'volume' ]
+            incoming_data['date_time']= incoming_data['date_time'].apply(lambda x: tmpstemp.fromtimestamp(x))
+            incoming_data.set_index('date_time', inplace=True)
+            self.update_history_from_df(incoming_data)
+            date_from = date_from + tmpdelta(days=190)
+        self.save_history_to_local()
+
+    def read_history_from_local(self):
+        incoming_data= pd.read_csv(self.local_path, sep='\t', index_col=0, parse_dates=True)
+        self.update_history_from_df(incoming_data)
+
+    def save_history_to_local(self):
+        try:
+            os.remove(self.local_path)
+        except OSError:
+            print('Error occured')
+            pass
+        self.history.to_csv(self.local_path, sep='\t', mode = 'w')
 
 class Portfolio:
     def __init__(self, origination_date = tmpstemp.fromisoformat('2000-01-01'), initial_deposit = 0):
