27cbb10185e7d9cbc0e598f324935bb0a6ebe253
Artem Lebedev <lebedevfedora@gmail.com>
2024-05-10 21:48:49 -0400

    Removed dependency on tensorflow

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,16 @@
-from portfolio import Portfolio, read_creds
+from portfolio import Portfolio
+import os
 
 def lambda_handler(event, context):
     rebal_portfolio = Portfolio()
-    
-    read_creds()
-    curr_pos = rebal_portfolio.get_current_postions()
 
-    for pos in curr_pos.index:
-        print(curr_pos['allocation'].loc[pos])
+    positions = rebal_portfolio.get_current_postions()
+    for ticker in positions.index:
+        alloc = positions['allocation'].loc[ticker]*100//1
+        if alloc >0:
+            print(f"{ticker}: {alloc}%")
+
+event_test = {'key1':'one', 'key2':'two'}
+context = 'QWERTY'
+
+lambda_handler(event_test, context)
