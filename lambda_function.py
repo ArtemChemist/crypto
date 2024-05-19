c52afde21c48c48145a1ec53dece92acb4d826cb
Artem Lebedev <lebedevfedora@gmail.com>
2024-05-19 11:08:44 -0400

    merged w main after portfolio takes asset dict

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,19 @@
 from portfolio import Portfolio
+from asset import Asset
 import os
 
 def lambda_handler(event, context):
-    rebal_portfolio = Portfolio()
+    BTC = Asset('BTC')
+    ETH = Asset('ETH')
+    USDT = Asset('USDT')
+    rebal_portfolio = Portfolio({'BTC':0, 'ETH':0, 'USDT':0})
 
     positions = rebal_portfolio.get_current_postions()
     for ticker in positions.index:
-        alloc = positions['allocation'].loc[ticker]*100//1
-        if alloc >0:
-            print(f"{ticker}: {alloc}%")
+        alloc = positions['allocation'].loc[ticker] *100
+        print(f"{ticker}: {alloc}%")
 
 event_test = {'key1':'one', 'key2':'two'}
 context = 'QWERTY'
-
+os.environ['MY_ENVIRONMENT'] = 'prod'
 lambda_handler(event_test, context)
--- portfolio.py
+++ portfolio.py
@@ -3,6 +3,7 @@ from pandas import Timestamp as tmpstemp
 from pandas import Timedelta as tmpdelta
 import os
 import json
+from asset import Asset
 
 from coinbase.rest import RESTClient
 if 'API_KEY' in os.environ:
@@ -18,107 +19,26 @@ else:
         print('Can not find keys')
 
 
-class Asset:
-    asset_dict = {}
-
-    @staticmethod
-    def make_USD():
-        USD = Asset('USD')
-        date_range = pd.date_range(start=tmpstemp.fromisoformat('2000-01-01'), end=tmpstemp.today())
-        idx =pd.DatetimeIndex(date_range, name = 'date_time')
-        USD_2000 = pd.DataFrame({'low':[1] * len(idx), 'high':[1] * len(idx),
-                            'open':[1] * len(idx), 'close':[1] * len(idx),
-                            'volume':[1000]* len(idx)},
-                            index=idx)
-        USD.update_history_from_df(USD_2000)
-        return USD
-
-    def __init__(self, ticker):
-        self.ticker = ticker
-        self.local_path = f'{self.ticker}_history.csv'
-        Asset.asset_dict.pop(ticker, None)
-        Asset.asset_dict[ticker] = self
-        self.history = pd.DataFrame(columns = ['high', 'low', 'open', 'close', 'volume'],
-                                    index = pd.DatetimeIndex([], name = 'date_time'))
-        try:
-            self.read_history_from_local()
-        except OSError:
-            pass
-
-        
-    def latest_price(self):
-        return self.price_on_date()
-    
-    def price_on_date(self, on_date = tmpstemp.today):
-        '''
-        Returns the prce on the date that is the closest to the supplied date
-        '''
-        matches = self.history.index.get_indexer([on_date], method='nearest')
-        matched_date = self.history.index[matches[0]]
-        if ((matched_date - on_date) > tmpdelta(days=1))  | ((on_date - matched_date) > tmpdelta(days=1)):
-            print(f'Reported {self.ticker} price is {on_date- matched_date} old')
-        return self.history['close'].loc[matched_date]
-    
-    def update_history_from_df(self, incoming_df:pd.DataFrame):
-        '''
-        incoming_df: the dataframe with the new historical records
-        '''
-        self.history = pd.concat([self.history, incoming_df]).groupby(level=0).last()
-        self.save_history_to_local()
-
-    def update_history_from_excahnge(self, currency: str, date_from, date_to):
-        '''
-        dates as pd.Datetime
-        currency: the currency we pay to obtain this asset, as a ticker (USD, USDT etc)
-        '''
-        next_date = date_from
-        while next_date < date_to:
-            date_from_unix = (date_from - tmpstemp("1970-01-01")) // tmpdelta('1s')
-            next_date = min((date_from + tmpdelta(days=190)), date_to)
-            next_date_unix = (next_date - tmpstemp("1970-01-01")) // tmpdelta('1s')
-
-            exchange_response = client.get_candles(f'{self.ticker}-{currency}', date_from_unix, next_date_unix, 'ONE_DAY')
-            incoming_data= pd.DataFrame( data = exchange_response['candles']) 
-            incoming_data.columns = [ 'date_time', 'low', 'high', 'open', 'close', 'volume' ]
-            incoming_data['date_time']= incoming_data['date_time'].apply(lambda x: tmpstemp.fromtimestamp(int(x)))
-            incoming_data.set_index('date_time', inplace=True)
-            self.update_history_from_df(incoming_data)
-            date_from = date_from + tmpdelta(days=190)
-        self.save_history_to_local()
-
-    def read_history_from_local(self):
-        incoming_data= pd.read_csv(self.local_path, sep='\t', index_col=0, parse_dates=True)
-        self.update_history_from_df(incoming_data)
-
-    def save_history_to_local(self):
-        try:
-            os.remove(self.local_path)
-        except OSError:
-            print('Error occured')
-            pass
-        self.history.to_csv(self.local_path, sep='\t', mode = 'w')
-
-class Portfolio:
-    def __init__(self, origination_date = tmpstemp.fromisoformat('2000-01-01'), initial_deposit = 0):
+class Portfolio_base:
+    def __init__(self):
         idx  = pd.MultiIndex(levels=[[],[]],
                           codes=[[],[]],
                           names=[u'date_time', u'ticker'])
         my_columns = [u'change', u'note']
         self.transactions  = pd.DataFrame(index=idx, columns=my_columns)
         self.value = pd.DataFrame(columns = ['value'], index = pd.DatetimeIndex([], name = 'date_time'))
-        self.orig_date = origination_date
-        self.update_transactions(transaction_date = origination_date,
-                                ticker = 'USD',
-                                qty = initial_deposit,
-                                note = 'Initial deposit')
-
 
     def update_transactions(self, ticker:str, qty:float, transaction_date = tmpstemp.today(), note = ''):
         self.transactions.loc[(transaction_date,ticker),:] = [qty, note]
 
+    def add_new_position(self, ticker:str, qty:float):
+        note = f'Added {ticker} as a new position'
+        self.update_transactions(ticker = ticker, qty = qty, note=note)
+    
     def get_hist_positions(self, on_date = tmpstemp.today()):
         '''
-        Returns portfolio composition on the specified date
+        Returns portfolio composition on the specified date, as a pandas df
+        Includs position size, value in USD and allocation
         '''
         # First, get the mask for all records before the date
         time_mask= self.transactions.index.get_level_values('date_time')<=on_date
@@ -142,6 +62,60 @@ class Portfolio:
             pass
         
         return positions
+
+class Portfolio_lambda(Portfolio_base):
+
+    def __init__(self, assets:dict):
+        super().__init__()
+        for account in client.get_accounts()['accounts']:
+            ticker = account['name'].split(' ')[0]
+            qty = float(account['available_balance']['value'])
+            if ticker in assets.keys():
+                self.add_new_position(ticker, qty)
+
+
+    def get_current_postions(self):
+        '''
+        Returns portfolio composition on the specified date, as a pandas df
+        Includs position size, value in USD and allocation
+        '''
+        # Filter the df with this mask, group by ticker
+        positions = self.transactions['change'].groupby(level = 'ticker').sum()
+        # Convert resulting Series to DataFrame
+        positions = pd.DataFrame(data = positions,
+                                index=pd.Index(positions.index, name = 'ticker'))
+        positions.columns = ['position_size']
+
+        for account in client.get_accounts()['accounts']:
+            ticker = account['name'].split(' ')[0]
+            qty = float(account['available_balance']['value'])
+            if ticker in positions.index:
+                positions.loc[ticker, 'position_size'] = qty
+
+        # Add the value in USD by multiplying on the asset price at this date
+        positions['position_value'] = positions.index.to_series().apply(
+                                                lambda x: Asset.asset_dict[str(x)].price_on_date()
+                                                )
+        positions['position_value'] = positions['position_value']*positions['position_size']
+        total_value = positions['position_value'].sum()
+        try:
+            positions['allocation'] = positions['position_value']/total_value
+        except Exception:
+            print('Error occured')
+            pass
+        
+        return positions
+
+class Portfolio_train(Portfolio_base):
+
+    def __init__(self, assets:dict, origination_date = tmpstemp.fromisoformat('2000-01-01') ):
+        super().__init__()
+        self.orig_date = origination_date
+        for asset in assets.items():
+            self.update_transactions(transaction_date = self.orig_date,
+                                    ticker = asset[0],
+                                    qty = asset[1],
+                                    note = 'Initial deposit')
 
     def get_hist_value(self, on_date = tmpstemp.today(), update = False):
         if update:
@@ -196,3 +170,12 @@ class Portfolio:
         self.value.sort_index(inplace=True)
 
     
+    
+class Portfolio(Portfolio_base):
+    
+    def __new__(cls, assets:dict, origination_date = tmpstemp.fromisoformat('2000-01-01')):
+        key_word = os.environ['MY_ENVIRONMENT']
+        if key_word == 'prod':
+            return Portfolio_lambda(assets = assets)
+        if key_word == 'training':
+            return Portfolio_train(assets = assets, origination_date = origination_date)
